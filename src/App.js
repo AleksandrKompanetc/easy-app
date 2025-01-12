6da46f9c5ba043792f5d1dbe2fc3408ae116e3e1
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-12 23:12:05 +0200

    displaying state of loading

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
+
   return (
     <div className="App">
       <header className='App-header'>
         <h1>Posts Viewer</h1>
+        {loading && <p>Loading posts...</p>}
+        {}
       </header>
     </div>
   );
