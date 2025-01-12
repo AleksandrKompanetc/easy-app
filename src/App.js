606441e656060d7fa2508b6a4a948d670ebf1498
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-12 23:29:15 +0200

    added changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(() => {
+    .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
