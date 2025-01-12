68f5410b7af867a443797512e9f0a2033716561a
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-12 23:10:09 +0200

    adding states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
   return (
     <div className="App">
-      
+      <header className='App-header'>
+        <h1>Posts Viewer</h1>
+      </header>
     </div>
   );
 }
