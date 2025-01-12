5c946d55cfe4f64fcc8bb0caaa0168b8fd0a0609
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-12 23:19:32 +0200

    creating fetch request

--- src/App.js
+++ src/App.js
@@ -6,6 +6,20 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(() => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json()
+    })
+    .then((data) => {
+      setPosts(data);
+      setLoading(false);
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className='App-header'>
